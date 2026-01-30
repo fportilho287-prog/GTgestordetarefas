045679e5896879af6ab6d5c7e41c41532cadee4c
Kadine515 <98369178+Kadine2511@users.noreply.github.com>
2026-01-29 23:20:26 -0400

    Add files via upload

---
+++ GTgestordetarefas-main/script.js
@@ -0,0 +1,9 @@
+function abrirJanela() {
+    document.getElementById('minhaJanela').style.display = 'block';
+    document.getElementById('overlay').style.display = 'block';
+  }
+
+  function fecharJanela() {
+    document.getElementById('minhaJanela').style.display = 'none';
+    document.getElementById('overlay').style.display = 'none';
+  }
